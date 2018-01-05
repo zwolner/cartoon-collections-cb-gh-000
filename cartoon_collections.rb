d598f8bbf34067458d97227aa8c7ac3333afeade
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 20:31:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  if words.all? { |e| e.size < 4}
+  words.all? { |e| e.size < 4}
     return false
   else
     return true
