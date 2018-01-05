29569fade630d50986cc7dd38b444545c7b07f37
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 20:06:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! {|i| return i.capitalize}+"!"}
+  planeteer_calls.map! {|i| i.capitalize}+"!"}
   # Your code here
 end
 
