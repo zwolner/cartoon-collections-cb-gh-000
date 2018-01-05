0125d2519d3a34fc94d1f1c0c2a7384d262ae860
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 20:03:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|x, index| puts "#{index+1} #{x}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
