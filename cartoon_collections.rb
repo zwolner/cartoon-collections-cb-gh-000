5dde87833324814a2ab69d554f2f3a3f651e1970
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 20:33:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.all? { |e| e.size < 4} else return false
+  words.all? { |e| e.size < 4}
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
